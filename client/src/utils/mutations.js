781d5cabb0dff7a256799bf324b58bf4c0e7238a
Julian Esquivel <julianesquivel@Julians-MacBook-Pro-2.local>
2022-05-24 03:17:32 -0400

    app.js updated

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,5 @@
 import gql from "graphql-tag";
-
+// LOGIN_USER will execute the loginUser mutation
 export const LOGIN_USER = gql`
     mutation login($email: String!, $password: String!) {
         login(email: $email, password: $password) {
@@ -11,7 +11,7 @@ export const LOGIN_USER = gql`
         }
     }
 `;
-
+// ADD_USER will execute the addUser mutation
 export const ADD_USER = gql`
     mutation addUser($username: String!, $email: String!, $password: String!) {
         addUser(username: $username, email: $email, password: $password) {
@@ -23,7 +23,7 @@ export const ADD_USER = gql`
         }
     }
 `;
-
+// SAVE_BOOK will execute the saveBook mutation
 export const SAVE_BOOK = gql`
     mutation saveBook($input: SavedBookInput) {
         saveBook(input: $input) {
@@ -40,7 +40,7 @@ export const SAVE_BOOK = gql`
         }
     }
 `;
-
+// REMOVE_BOOK will execute the removeBook mutation
 export const REMOVE_BOOK = gql`
     mutation removeBook($bookId: String!) {
         removeBook(bookId: $bookId) {
